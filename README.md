fc825cde201cd5f68b571219ef254ffa47b980bf
gruningzen-vi-leno <kimono76.live@gmail.com>
2022-02-28 12:23:12 -0300

    doc:readme

---
+++ README.md
@@ -0,0 +1,6 @@
+#ENTITY FINDER USING MACHINE LEARNING 
+
+```python
+	python -m  spacy download en_core_web_sm
+
+```
---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name="entity-finder",
+    version="0.0.1",
+    descriptio="flask project using spaCy"
+)
