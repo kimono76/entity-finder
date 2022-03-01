0345a7edf07ee04596944d87230826b66ea8e3d9
gruningzen-vi-leno <kimono76.live@gmail.com>
2022-03-01 18:32:15 -0300

    test:client pass

---
+++ client.py
@@ -0,0 +1,6 @@
+class NamedEntityRecognitionClient:
+    def __init__(self):
+        pass 
+
+    def getEntities(self, text):
+        return {}
--- test/test_client.py
+++ test/test_client.py
@@ -1,4 +1,14 @@
 import unittest as ut
+from client import NamedEntityRecognitionClient
 
 class TestClient(ut.TestCase):
-    
+    def test_getEntities_emptyString_dictionary(self):
+        client = NamedEntityRecognitionClient()
+        entities = client.getEntities("")
+        self.assertIsInstance(entities, dict)
+
+# dictionary of entities
+#{
+# ents: [{...}],
+# html: "<span>..."
+# }
