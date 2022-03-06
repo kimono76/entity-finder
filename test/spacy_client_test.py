98cd3e5f56720e3f8d39fd127d1a614a9b8cc26b
qimono-desk-fedora <kimono76.live@gmail.com>
2022-03-06 18:25:16 -0300

    test:spacy client test double pass

---
+++ test/spacy_client_test.py
@@ -0,0 +1,46 @@
+class NamedEntityRecognitionClient_Test:
+    """
+    test_doubles for spaCy NLP model
+
+    for using mock class implementations you could also use the following:
+    
+    pytest_mock 
+    pip install pytest-mock
+    https://medium.com/@bfortuner/python-unit-testing-with-pytest-and-mock-197499c4623c
+    https://www.freblogg.com/pytest-functions-mocking-1
+
+    monkey patch
+    https://www.geeksforgeeks.org/monkey-patching-in-python-dynamic-behavior/
+    https://stackoverflow.com/questions/5626193/what-is-monkey-patching
+
+
+    """
+
+    def __init__(self, model):
+        self.model = model
+    
+    def returns_doc_ents(self,entities):
+        self.entities = entities 
+
+    def __call__(self, sentence):
+        return DocTest(sentence, self.entities)
+    
+class DocTest:
+    """
+    class DocTestDouble Test double for spaCy Doc
+    """
+    def __init__(self, sent, entities):
+        self.entities = [SpanTest(ent['text'], ent['label_']) for ent in entities]
+
+    def patch_methods(self, attr, return_value):
+        def patched(): return return_value
+        setattr(self, attr, patched)
+        return self
+
+class SpanTest:
+    """
+    class SpanTestDouble for spacy span
+    """
+    def __init__(self, text, label):
+        self.text = text
+        self.label = label
--- test/test_client.py
+++ test/test_client.py
@@ -1,10 +1,12 @@
 import unittest as ut
 from client import NamedEntityRecognitionClient
+from spacy_client_test import NamedEntityRecognitionClient_Test
 
 class TestClient(ut.TestCase):
-    def test_getEntities_emptyString_dictionary(self):
-        model= NamedEntityRecognitionClient_Test_Double('eng')
-        client = NamedEntityRecognitionClient()
+    def test_getEntities_emptyString_emptySpacyDocEntities(self):
+        model= NamedEntityRecognitionClient_Test('eng')
+        model.returns_doc_ents([])
+        client = NamedEntityRecognitionClient(model)
         entities = client.getEntities("")
         self.assertIsInstance(entities, dict)
 
