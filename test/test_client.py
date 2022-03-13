fa7fe07122f011842b9910616edb729055ce9eab
vi-ins <kimono76.live@gmail.com>
2022-03-13 16:40:45 -0300

    test:ents

--- test/test_client.py
+++ test/test_client.py
@@ -1,13 +1,14 @@
+from html import entities
 import unittest as ut
 from spacy_client import NamedEntityRecognitionClient
-from client_double import NamedEntityRecognitionClient_double
+from spacy_client_double import NamedEntityRecognitionClient_double
 
 class TestClient(ut.TestCase):
     def test_getEntities_emptyString_emptySpacyDocEntities(self):
         model= NamedEntityRecognitionClient_double('eng')
-        model.returns_doc_ents([])
-        client = NamedEntityRecognitionClient(model)
-        entities = client.getEntities("")
+        entities= model.returns_doc_ents([])
+        #client = NamedEntityRecognitionClient(model)
+        #entities = client.getEntities("")
         self.assertIsInstance(entities, dict)
 
     # def test_getEntities_string_list(self):
