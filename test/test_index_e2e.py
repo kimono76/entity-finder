12936b552a3bf7fcd93f9b155c1846ddae3e0cc9
AlbertoGruning-MacOS <kimono76.live@gmail.com>
2022-09-11 18:29:40 -0300

    test:import os for locating file

--- test/test_index_e2e.py
+++ test/test_index_e2e.py
@@ -1,10 +1,23 @@
+import os
 import unittest
 from selenium import webdriver
 
 class EndToEndTests(unittest.TestCase):    
     
     def setUp(self):
-        SELENIUM_DRIVER_LOCATION = r'/Application/geckodriver'
+        CURRENT_WORKING_DIRECTORY = os.getcwd()
+        selenium_dirver_location_in_project = 'application/firefox/macos'
+        selenium_driver_filename = 'geckodriver'
+        
+        print('Current Working Directory: ')
+        print(CURRENT_WORKING_DIRECTORY)
+        
+        SELENIUM_DRIVER_LOCATION = os.path.join(
+            CURRENT_WORKING_DIRECTORY,
+            selenium_dirver_location_in_project,
+            selenium_driver_filename
+        )
+        #SELENIUM_DRIVER_LOCATION = r'/Application/geckodriver'
         
         FLASK_LOCAL_PORT = 'http://localhost:5000'
         FLASK_STAGING_PORT = 'https://'
