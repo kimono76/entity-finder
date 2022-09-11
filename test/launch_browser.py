c4c683f4bc79c64571f1798f4780b7601473bdbc
AlbertoGruning-MacOS <kimono76.live@gmail.com>
2022-09-11 17:50:09 -0300

    docs:corrections in leuch browser sample for executionin macos

--- test/launch_browser.py
+++ test/launch_browser.py
@@ -5,19 +5,21 @@ from selenium.webdriver.firefox.options import Options
 
 # source tutorial https://youtu.be/kpONBQ3muLg 
 
-firefox_user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:104.0) Gecko/20100101 Firefox/104.0'
+macos_firefox_user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:104.0) Gecko/20100101 Firefox/104.0'
 
 firefox_driver = os.path.join(
     os.getcwd(),
-    'selenium-drivers/firefox/macos',
+    'test/selenium-drivers/firefox/macos',
     'geckodriver'
     )
 
 firefox_service = Service(firefox_driver)
 firefox_options = Options()
-firefox_options.set_preference('general.useragent.override',firefox_user_agent)
+firefox_options.set_preference('general.useragent.override',macos_firefox_user_agent)
 
 # launch firefox browser 
 browser = webdriver.Firefox(service=firefox_service,options=firefox_options)
 browser.get('https://python.org')
 
+# run this module from ~/source/repos/entity-finder
+# python ./test/launch_browser.py
