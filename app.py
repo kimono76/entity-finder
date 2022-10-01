214bbdeee474800d957bfceea387c4e88d6bf422
AlbertoGruning-MacOS <kimono76.live@gmail.com>
2022-10-01 17:35:10 -0300

    test: pass for response ok

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
+from crypt import methods
+import json
+from urllib import response
 from flask import Flask, render_template, request
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/ner',methods=['POST'])
+def request_named_entities():
+    data = request.get_json()
+    response = True 
+    return json.dumps(response)
 
 if __name__ =='__main__':
     app.run(debug=True)
